a847cb926b525afca5966088c1694a14c654f2f6
Michael Eisenbraun <eisenbm@algonquincollege.edu>
2019-07-24 13:24:01 -0400

    Adds starter files

---
+++ queries.sql
@@ -0,0 +1,18 @@
+-- 1. Add a movie to the `movies` table with the following data: movie_title = 'WALL-E', director = 'Brad Bird', year = 2008, genre_id = 2
+
+
+
+-- 2. Using a subquery, add a movie to the `movies` table with the following data: movie_title = 'Inside Out', director = 'Peter Doctor', year = 2014, genre_title = 'Family'
+
+
+
+-- 3. Update the year (`year`) to `2015` for the movie with the title (`movie_title`) 'Inside Out'.
+
+
+
+-- 4. Using a subquery, update the director (`director`) to 'Peter Doctor' and the genre to 'Romance' for the movie with the title (`movie_title`) 'WALL-E'.
+
+
+
+-- 5. Remove all the movies from the director (`director`) 'Peter Doctor'
+
