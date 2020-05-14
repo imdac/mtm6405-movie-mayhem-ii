2880d9d03b118f45b30ff0d2a48defe70d2f0bdd
Michael Eisenbraun <eisenbm@algonquincollege.edu>
2020-05-14 14:53:56 -0400

    Combines MM II and MM III

--- queries.sql
+++ queries.sql
@@ -16,3 +16,22 @@
 
 -- 5. Remove all the movies from the director (`director`) 'Peter Doctor'
 
+
+
+-- 6. Using an INNER JOIN, retrieve the movie title (`movie_title`), year (`year`), and genre title (`genre_title`) for all the movies in the `movies` table. 
+
+
+
+-- 7. Retrieve the total number of movies in the `movies` table with the heading `Number of Movies`.
+
+
+
+-- 8. Retrieve the oldest movie year in the `movies` table in the genre 'Fantasy'.
+
+
+
+-- 9. Retrieve the number of movies for each genre title. The headings should be `Genre` and `Number of Movies`.
+
+
+
+-- 10. Retrieve a list of directors that have more than 1 movie in the `movies` table.
